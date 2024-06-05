f2b57d13b98d7a86290498426c66172e2498f565
Imran00852 <bhatimran9797@gmail.com>
2024-06-05 21:31:29 +0530

    added patterns

---
+++ Patterns/Pattern4.java
@@ -0,0 +1,27 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern4 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        int space=0;
+        int star=n;
+        for (int i=1;i<=n;i++){
+
+            //space
+            for(int j=1;j<=space;j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int k=1;k<=star;k++){
+                System.out.print("*");
+            }
+            space++;
+            star--;
+            System.out.println();
+        }
+    }
+}
